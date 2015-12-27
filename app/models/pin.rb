4b5cdc63d94908382acf63b3f39b41fd027c45f3
Zach Rose <zachrose24@gmail.com>
2015-12-27 16:28:45 -0600

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ belongs_to :user
 has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+validates :image, presence: true
+validates :description, presence: true
 end
